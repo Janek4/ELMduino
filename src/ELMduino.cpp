06b8a10bcb1df3e133450c3b835d996460bd753c
Janek4 <81467277+Janek4@users.noreply.github.com>
2024-02-04 03:09:25 +0100

    Update ELMduino.cpp
    hokus

--- src/ELMduino.cpp
+++ src/ELMduino.cpp
@@ -1968,6 +1968,35 @@ float ELM327::fuelRate()
     return processPID(SERVICE_01, ENGINE_FUEL_RATE, 1, 2, 1.0 / 20.0);
 }
 
+
+float ELM327::engineFuelConsumption() {
+    return processPID(SERVICE_01, ENGINE_FUEL_CONSUPTION, 1, 2, 1.0 / 20.0);
+}
+float ELM327::avgReagentConsumption1() {
+    return processPID(SERVICE_01, REAGENT_AVG_CONSUPTION_1, 1, 2, 1.0 / 20.0);
+}
+float ELM327::avgReagentConsumption2() {
+    return processPID(SERVICE_01, REAGENT_AVG_CONSUPTION_2, 1, 2, 1.0 / 20.0);
+}
+float ELM327::instantFuelConsumptionCalc1() {
+    return processPID(SERVICE_01, INSTANT_FUEL_CONSUMPTION_CALC_1, 1, 2, 1.0 / 20.0);
+}
+float ELM327::instantFuelConsumptionCalc2() {
+    return processPID(SERVICE_01, INSTANT_FUEL_CONSUMPTION_CALC_2, 1, 2, 1.0 / 20.0);
+}
+float ELM327::avgFuelConsumption() {
+    return processPID(SERVICE_01, AVG_FUEL_CONSUPTION, 1, 2, 1.0 / 20.0);
+}
+float ELM327::avgFuelConsumptionTotal() {
+    return processPID(SERVICE_01, AVG_FUEL_CONSUPTION_TOTAL, 1, 2, 1.0 / 20.0);
+}
+float ELM327::avgFuelConsumption10sec() {
+    return processPID(SERVICE_01, AVG_FUEL_CONSUPTION_10_SEC, 1, 2, 1.0 / 20.0);
+}
+
+
+
+
 /*
  uint8_t ELM327::emissionRqmts()
 
